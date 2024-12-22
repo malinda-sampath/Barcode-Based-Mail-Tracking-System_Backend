c3993ba16b9f622d1c800452fba29312c970c4f8
malinda-sampath <malindasampath45@gmail.com>
2024-12-23 01:44:42 +0530

    Advisor package

---
+++ UoR_MTS-Backend/mail-tracking-system/src/main/java/com/UoR_MTS_Backend/mail_tracking_system/advisor/ExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.UoR_MTS_Backend.mail_tracking_system.advisor;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+}
