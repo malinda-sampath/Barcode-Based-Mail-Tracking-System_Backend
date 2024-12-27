5fedcfd76e01567ee93da9571ab77ed95d9ae340
Sanjuka Sri Nuwan <151758100+SNuwan00@users.noreply.github.com>
2024-12-27 14:25:45 +0530

    branch controller file

--- UoR_MTS-Backend/mail-tracking-system/src/main/java/com/UoR_MTS_Backend/mail_tracking_system/controller/BranchController.java
+++ UoR_MTS-Backend/mail-tracking-system/src/main/java/com/UoR_MTS_Backend/mail_tracking_system/controller/BranchController.java
@@ -1,9 +1,21 @@
 package com.UoR_MTS_Backend.mail_tracking_system.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.UoR_MTS_Backend.mail_tracking_system.dto.BranchDTO;
+import com.UoR_MTS_Backend.mail_tracking_system.service.BranchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
+@RequestMapping("api/branch")
 public class BranchController {
+
+    @Autowired
+    private BranchService branchService;
+
+    @PostMapping("/save")
+    public String branckSave(@RequestBody BranchDTO branchDTO){
+        branchService.branchSave(branchDTO);
+        return null;
+    }
 }
--- UoR_MTS-Backend/mail-tracking-system/src/main/java/com/UoR_MTS_Backend/mail_tracking_system/service/impl/BranchServiceIMPL.java
+++ UoR_MTS-Backend/mail-tracking-system/src/main/java/com/UoR_MTS_Backend/mail_tracking_system/service/impl/BranchServiceIMPL.java
@@ -1,8 +1,27 @@
 package com.UoR_MTS_Backend.mail_tracking_system.service.impl;
 
+import com.UoR_MTS_Backend.mail_tracking_system.dto.BranchDTO;
+import com.UoR_MTS_Backend.mail_tracking_system.model.Branch;
+import com.UoR_MTS_Backend.mail_tracking_system.repo.BranchRepo;
 import com.UoR_MTS_Backend.mail_tracking_system.service.BranchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BranchServiceIMPL implements BranchService {
+
+    @Autowired
+    private BranchRepo branchRepo;
+
+    @Override
+    public String branchSave(BranchDTO branchDTO) {
+        Branch branch = new Branch(
+                branchDTO.getBranchCode(),
+                branchDTO.getBranchName(),
+                branchDTO.getInsertDate(),
+                branchDTO.getUpdateDate()
+        );
+        branchRepo.save(branch);
+        return null;
+    }
 }
