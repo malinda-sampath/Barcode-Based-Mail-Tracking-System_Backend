0c3aa5fb8876f7c25f3b45fd3ccdfb3e12df9194
Manuri-Rasara <manurirasarahewage@gmail.com>
2025-01-21 23:17:44 +0530

    CLD_0001_vii

--- UoR_MTS-Backend/mail-tracking-system/src/main/java/com/UoR_MTS_Backend/mail_tracking_system/controller/EmailVerificationController.java
+++ UoR_MTS-Backend/mail-tracking-system/src/main/java/com/UoR_MTS_Backend/mail_tracking_system/controller/EmailVerificationController.java
@@ -2,8 +2,11 @@ package com.UoR_MTS_Backend.mail_tracking_system.controller;
 
 import com.UoR_MTS_Backend.mail_tracking_system.dto.EmailVerificationResponseDTO;
 import com.UoR_MTS_Backend.mail_tracking_system.service.EmailVerificationService;
+import com.UoR_MTS_Backend.mail_tracking_system.utill.response.ResponseBuilder;
+import com.UoR_MTS_Backend.mail_tracking_system.utill.response.StandardResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +20,15 @@ public class EmailVerificationController {
     private final EmailVerificationService emailVerificationService;
 
     @PostMapping("/verify/{email}")
-    public EmailVerificationResponseDTO verifyEmail(
-            @PathVariable String email
-    ) {
-
+    public ResponseEntity<StandardResponse<EmailVerificationResponseDTO>>  verifyEmail(@PathVariable String email) {
+            try{
             //message should be "OTP sent successfully"
-            return emailVerificationService.verifyEmail(email);
+                EmailVerificationResponseDTO Message = emailVerificationService.verifyEmail(email);
+                return ResponseBuilder.success("OTP Send Successfully",null);
+            }catch(Exception e){
+                return ResponseBuilder.error("Error Occure When OTP Send",null);
+            }
+
+
     }
 }
