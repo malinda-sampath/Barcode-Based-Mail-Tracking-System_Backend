a652cc6f50e5a0e507763a6a4dda2d24066d7ae4
malinda-sampath <malindasampath45@gmail.com>
2025-02-16 20:18:32 +0530

    CLS_0002-iv

--- UoR_MTS-Backend/mail-tracking-system/src/main/java/com/UoR_MTS_Backend/mail_tracking_system/controllers/PasswordController.java
+++ UoR_MTS-Backend/mail-tracking-system/src/main/java/com/UoR_MTS_Backend/mail_tracking_system/controllers/PasswordController.java
@@ -1,11 +1,12 @@
 package com.UoR_MTS_Backend.mail_tracking_system.controllers;
 
+import com.UoR_MTS_Backend.mail_tracking_system.dtos.RequestPasswordResetDTO;
 import com.UoR_MTS_Backend.mail_tracking_system.services.PasswordService;
+import com.UoR_MTS_Backend.mail_tracking_system.utils.response.ResponseBuilder;
+import com.UoR_MTS_Backend.mail_tracking_system.utils.response.StandardResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/password")
@@ -14,9 +15,26 @@ public class PasswordController {
 
     private final PasswordService passwordService;
 
-    @PostMapping("/reset/{email}")
-    public String getOTP(@RequestBody String email){
-        String message = passwordService.getOTP(email);
-        return message;
+    @PostMapping("/get-otp/{email}")
+    public ResponseEntity<StandardResponse<String>> getOTP(@PathVariable String email) {
+
+        try {
+            String OTP = passwordService.getOTP(email);
+            return ResponseBuilder.success("OTP sent successfully", OTP);
+        } catch (Exception e) {
+            return ResponseBuilder.error(e.getMessage(), null);
+        }
     }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<StandardResponse<String>> resetPassword(@RequestBody RequestPasswordResetDTO requestPasswordResetDTO) {
+
+        try {
+            String message = passwordService.resetPassword(requestPasswordResetDTO);
+            return ResponseBuilder.success(message, null);
+        } catch (Exception e) {
+            return ResponseBuilder.error(e.getMessage(), null);
+        }
+    }
+
 }
