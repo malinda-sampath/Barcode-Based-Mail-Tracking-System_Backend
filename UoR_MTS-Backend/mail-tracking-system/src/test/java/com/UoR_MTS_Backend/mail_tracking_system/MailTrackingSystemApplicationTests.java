a6b7d81108a63188432fab03a0413e9c43732fad
malinda-sampath <malindasampath45@gmail.com>
2025-04-05 02:20:46 +0530

    CLD_0006-ii

--- UoR_MTS-Backend/mail-tracking-system/src/test/java/com/UoR_MTS_Backend/mail_tracking_system/MailTrackingSystemApplicationTests.java
+++ UoR_MTS-Backend/mail-tracking-system/src/test/java/com/UoR_MTS_Backend/mail_tracking_system/MailTrackingSystemApplicationTests.java
@@ -2,12 +2,15 @@ package com.UoR_MTS_Backend.mail_tracking_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MailTrackingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
